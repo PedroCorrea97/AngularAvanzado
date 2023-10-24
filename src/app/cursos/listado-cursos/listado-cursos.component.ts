20b5a20a3e5b01a49ba490e5005916a53481bc2d
DESKTOP-VM20CSM\Carlos Rivera <carrivera@uv.mx>
2023-10-24 09:36:46 -0600

    Select

--- src/app/cursos/listado-cursos/listado-cursos.component.ts
+++ src/app/cursos/listado-cursos/listado-cursos.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CategoriasService } from 'src/app/services/categorias/categorias.service';
+import { CursosService } from 'src/app/services/cursos/cursos.service';
 
 @Component({
   selector: 'app-listado-cursos',
   templateUrl: './listado-cursos.component.html',
   styleUrls: ['./listado-cursos.component.scss']
 })
-export class ListadoCursosComponent {
+export class ListadoCursosComponent implements OnInit {
+  listadoCategorias : any;
+  dataSource:any;
+  displayedColumns = ['id', 'nombre','descripcion','acciones'];
+  pageRegister = 5;
+  constructor(private categoriesService : CategoriasService, private cursosService : CursosService) {
+  }
+
+  ngOnInit(){
+    this.initSelect();
+    this.initeTable();
+  }
+
+  private initSelect(){
+    this.categoriesService.getAllCategories().subscribe((resp) => {
+      this.listadoCategorias = resp; });
+  }
+
+  private initeTable(){
+    this.cursosService.getAllCourses().subscribe((resp) => {
+      this.dataSource = resp; });
+  }
 
 }
