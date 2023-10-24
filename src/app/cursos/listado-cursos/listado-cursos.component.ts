96e67fd98dca019755a830d0820bfa3d18b7656a
Pedro Correa <pedro_correa23@hotmail.com>
2023-10-24 09:38:47 -0600

    Fix Table Courses

--- src/app/cursos/listado-cursos/listado-cursos.component.ts
+++ src/app/cursos/listado-cursos/listado-cursos.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, ViewChild } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { CursosService } from 'src/app/services/cursos/cursos.service';
 
 @Component({
   selector: 'app-listado-cursos',
@@ -6,5 +10,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./listado-cursos.component.scss']
 })
 export class ListadoCursosComponent {
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  dataSource : any;
+
+  // Columnas tabla categorias
+  displayedColumns = ['id', 'nombre','descripcion','acciones'];
+  pageRegister = 5;
+  
+  constructor(
+    private coursesService : CursosService,
+    private cdr: ChangeDetectorRef,
+    private dialog : MatDialog,
+    private snackBar : MatSnackBar){
+      
+    }
+    ngOnInit() {
+      this.chargeCat();
+    }
+    
+    private chargeCat() { this.coursesService.getAllCourses().subscribe((resp) => {
+      this.dataSource = resp; });
+    }
 
 }
